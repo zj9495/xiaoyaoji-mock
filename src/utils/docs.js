670e5ddff4214117782006e8caa56539ae5074db
尹程程 <yincc@idss-cn.com>
2020-10-28 11:37:17 +0800

    feat: 优化文档支持

--- src/utils/docs.js
+++ src/utils/docs.js
@@ -1,10 +1,25 @@
 const axios = require("axios")
 
+const parseDoc = (docs, result = []) => {
+  docs.forEach(item => {
+    if (item.content) {
+      result.push(JSON.parse(item.content))
+    }
+    if (item.children && item.children.length) {
+      parseDoc(item.children, result)
+    }
+  })
+  return result
+}
+
 const getDoc = async (url) => {
   const res = await axios.get(url)
-  console.log('res: ', res.data.docs[0].children);
+  const doc = []
 
-  return res.data.docs[0].children.map(item => (JSON.parse(item.content)))
+  // const result = res.data.docs[0].children.map(item => (JSON.parse(item.content)))
+  const result = parseDoc(res.data.docs)
+  console.log('result: ', result);
+  return result
   return res.data.docs[0].children.reduce((pre, cur) =>  pre.children.concat(cur.children)).map(item => (JSON.parse(item.content)))
 }
 
