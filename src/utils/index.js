9780a00f3733588257acd7e0c2062bd2308249c2
尹程程 <yincc@idss-cn.com>
2020-11-03 18:06:29 +0800

    feat: support arrary

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,15 +3,16 @@ const {getFormat} = primitives
 
 const getStructure = (data, obj = {}) => {
   data.forEach(item => {
-    const { type, name, children = [] } = item
+    let { type, name, children = [] } = item
     let res
     if (children.length) {
       res = getStructure(children)
       if (type.startsWith("array")) {
         res = [res]
+        name = name + '|1-20'
       }
     } else {
-      const format = getFormat(name)
+      const format = getFormat(name, type)
       res = primitives[type + "_" + format] || primitives[type] || primitives["_default"]
     }
     obj[name] = res
